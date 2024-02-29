97282448dc82e9bdaef4bb99207179a9285d79a7
Dominic <xuanvinguyen2003@gmail.com>
2024-02-29 08:12:10 +0700

    setting up the structure for the game
    write some guideline functions for r-p-s game

---
+++ script.js
@@ -0,0 +1,18 @@
+//function to randomly get a value either rock, paper or scissors
+function getComputerChoice() {
+
+}
+
+//function to get user input, bypass case sensitive inputs and uppercase them 
+function getPlayerChoice () {
+
+}
+
+//values that is used in the game round that is empty for now  
+const playerSelection = ""
+const computerSelection = ""
+
+//function to play a r-p-s round
+function gameRound (playerSelection, computerSelection) {
+
+}
